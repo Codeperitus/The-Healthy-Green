291c434e5f366724d22771112e4072a170635534
Jaskamal <Jaskamalcw@gmail.com>
2014-08-23 14:17:52 +0530

    basic setup

---
+++ app/code/local/AgileContinental/Module/Block/Test.php
@@ -0,0 +1,5 @@
+<?php
+class AgileContinental_Module_Block_Test extends Mage_Core_Block_Template 
+{    
+    
+}
---
+++ app/code/local/AgileContinental/Module/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class AgileContinental_Module_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo 'Hello developer...';
+    }
+	public function testAction()
+    {
+        echo 'Test developer...';
+    }
+}
+?>
